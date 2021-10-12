a60a7401593cc19ebd204adb9b4d8345cf532e02
Ian <10962612@uvu.edu>
2021-10-12 15:03:37 -0600

    New Code, Shooty McShoot.

--- 3DFirstPerson/3DFirstPersonDemo/Assets/Scripts/PlayerControl.cs
+++ 3DFirstPerson/3DFirstPersonDemo/Assets/Scripts/PlayerControl.cs
@@ -11,7 +11,8 @@ public class PlayerControl : MonoBehaviour
     public float minLookX;
     private float rotX;
     private Camera camera;
-    private Ridgidbody rb;
+    private Rigidbody rb;
+    private WeaponReady weapon;
 //Move in unit per sec
 //force upwards
 //Mouse Look Sensitive
@@ -19,31 +20,63 @@ public class PlayerControl : MonoBehaviour
 //Highest up
 //Current x rotate
 
+    void Awake()
+    {
+        weapon = GetComponent<WeaponReady>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
         //get components
-        camera = camera.main;
-        rig = GetComponent<Rigidbody>();
+        camera = Camera.main;
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        Move();
+        CamLook();
         
+        if(Input.GetButton("Fire1"))
+        {
+            if(weapon.CanShoot())
+                weapon.Shoot();
+        }
     }
 
+    void FixedUpdate()
+    {
+        if(Input.GetButtonDown("Jump"))
+            Jump();
+    }
     void Move()
     {
         float x = Input.GetAxis("Horizontal") * moveSpeed;
-        float y = Input.GetAxis("Vertical") * moveSpeed;
+        float z = Input.GetAxis("Vertical") * moveSpeed;
 
-        rb.velocity = new Vector3(x, rb.velocity.y, z);
+        // rb.velocity = new Vector3(x, rb.velocity.y, z);
+        Vector3 dir = transform.right * x + transform.forward * z;
+        rb.velocity = dir;
     }
 
     void CamLook()
     {
         float y =Input.GetAxis("Mouse X") * lookSensitivity;
         rotX += Input.GetAxis("Mouse Y") * lookSensitivity;
+
+        rotX = Mathf.Clamp(rotX, minLookX, maxLookX);
+        camera.transform.localRotation = Quaternion.Euler(-rotX, 0, 0);
+        transform.eulerAngles += Vector3.up * y;
+    }
+
+    void Jump()
+    {
+        Ray ray = new Ray(transform.position, Vector3.down);
+
+        if(Physics.Raycast(ray, 1.1f))
+            rb.AddForce (Vector3.up * jumpForce, ForceMode.Impulse);
     }
 }
+
---
+++ 3DFirstPerson/3DFirstPersonDemo/Assets/Scripts/WeaponReady.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponReady : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public Transform muzzle;
+    public int curAmmo;
+    public int maxAmmo;
+    public bool infiniteAmmo;
+    public float bulletSpeed;
+    public float shootRate;
+    private float lastShootTime;
+    private bool isPlayer;
+
+    void Awake()
+    {
+        if(GetComponent<isPlayer>())
+            isPlayer = true;
+    }
+
+    public bool CanShoot()
+    {
+        if(Time.time - lastShootTime >= shootRate)
+        {
+            if(curAmmo > 0 || infiniteAmmo == true)
+                return true;
+        }
+
+        return false;
+
+    }
+
+    public void Shoot()
+    {
+        lastShootTime = lastShootTime.time;
+        curAmmo--;
+
+        GameObject bullet = Instantiate(bulletPrefab, muzzle.position, muzzle.rotation);
+        bullet.GetComponent<Rigidbody>().velocity = muzzle.forward * bulletSpeed;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
