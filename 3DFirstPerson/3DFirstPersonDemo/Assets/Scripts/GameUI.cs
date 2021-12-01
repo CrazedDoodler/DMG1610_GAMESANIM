85955bba5c43f56c134f60a80a8bcdaf064fa94a
Ian <10962612@uvu.edu>
2021-12-01 14:15:50 -0700

    Newest of New

---
+++ 3DFirstPerson/3DFirstPersonDemo/Assets/Scripts/GameUI.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    [Header("HUD")]
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI ammoText;
+    public Image healthBarFill;
+
+    [Header("Pause Menu")]
+    public GameObject Pause;
+    
+    [Header("End Game Screen")]
+    public GameObject End;
+    public TextMeshProUGUI EndGameHeadText;
+    public TextMeshProUGUI FinalScore;
+
+    //instance / singleton
+
+    public static GameUI instance;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- 3DFirstPerson/3DFirstPersonDemo/Assets/Scripts/PlayerControl.cs
+++ 3DFirstPerson/3DFirstPersonDemo/Assets/Scripts/PlayerControl.cs
@@ -88,6 +88,16 @@ public class PlayerControl : MonoBehaviour
             rb.AddForce (Vector3.up * jumpForce, ForceMode.Impulse);
     }
 
+    public void GiveHealth(int amountToGive)
+    {
+        curHP = Mathf.Clamp(curHP + amountToGive, 0, maxHP);
+    }
+
+    public void GiveAmmo(int amountToGive)
+    {
+        weapon.curAmmo = Mathf.Clamp(weapon.curAmmo + amountToGive, 0, weapon.maxAmmo);
+    }
+
     public void TakeDamage(int damage)
     {
         curHP -= damage;
